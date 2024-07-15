ea7d533b7a766cc513783327f4c39342c473ac1c
mahesh06111999 <maheshsr611@gmail.com>
2024-07-15 16:33:55 +0530

    Done

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+const db = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  database: process.env.MYSQL_DB,
+  password: process.env.MYSQL_PASSWORD,
+  port: process.env.MYSQL_PORT,
+});
+
+module.exports = db;
---
+++ utils/eventEmitter.js
@@ -0,0 +1,5 @@
+const EventEmitter = require('events');
+const orderPlaced = new EventEmitter();
+const bookAdded = new EventEmitter();
+
+module.exports = { orderPlaced, bookAdded };
